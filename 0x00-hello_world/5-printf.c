83d6230ff7eb22f3864cc9c358858b2d4eedd94f
sechabantsioa <sechabantsioa@gmail.com>
2022-10-16 01:27:25 -0700

    Task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 /**
- * main - print a c program using printf
- *
- * Return : 0
-*/
+ * main - Entry
+ * Return: Always 0
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
