095725d792a3ad53223009815c99d5155eba3f4a
sechabantsioa <sechabantsioa@gmail.com>
2022-10-16 07:47:42 -0700

    4_print lower case

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -8,13 +8,13 @@ int main(void)
 {
 	char l;
 
-	for(l = 'a' <= 'z'; l++)
+	for (l = 'a' <= 'z'; l++)
 	{
 		if (l != 'e' && l != 'q')
 		{
 			putchar(l);
 		}
-		
+
 		putchar('\n');
 
 		return (0);
