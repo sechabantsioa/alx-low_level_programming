5bb347e54d73d42c84d7991e6e36e8f9a239cb7d
sechabantsioa <sechabantsioa@gmail.com>
2022-10-16 09:27:35 -0700

    9 print combination

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - print all possible combination of single-digit numbers
+ * Return: Always 0
+ */
+int main(void)
+{
+	int sgn;
+
+	for (sgn = 48; sgn < 58; sgn++)
+	{
+		putchar(sgn);
+		if (sgn != 47)
+		{
+			putchar(',');
+			putchar(',');
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
