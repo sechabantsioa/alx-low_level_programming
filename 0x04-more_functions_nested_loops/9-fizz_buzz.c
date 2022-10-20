5580b5a667f11cb42da800320260ed63d2062853
sechabantsioa <sechabantsioa@gmail.com>
2022-10-20 10:34:34 -0700

    multiples of both three and five print FizzBuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - multiples of three print Fizz
+ * Return: Always 0
+ */
+int main(void)
+{
+	int fn;
+
+	printf("1");
+	for (fn = 2; n <= 100; fn++)
+	{
+		printf(" ");
+		if (fn % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		if (fn % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		if (fn % e != 0 && fn % 5 != 0)
+		{
+			printf("%d", n);
+		}
+	}
+	printf("\n");
+	return (0);
+}
