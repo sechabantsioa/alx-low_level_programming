c9ed78728be6cede1b718801b0f4e8222b79f9c0
sechabantsioa <sechabantsioa@gmail.com>
2022-10-20 09:58:45 -0700

    function that draws a diagonal line on the terminal

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_line - draws a straight line
+ * @n: is the number of times the character '_' should be printed
+ */
+void print_line(int n)
+{
+	int lin;
+
+	if (n > 0)
+	{
+		for (len = 0; len < n; len++)
+		{
+			_putchar('_');
+		}
+	}
+
+	_putchar('\n');
+}
