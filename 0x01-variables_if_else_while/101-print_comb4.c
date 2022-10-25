46389eee3e5e731cf5a72440a0bf138300a7f580
sechabantsioa <sechabantsioa@gmail.com>
2022-10-25 12:47:00 -0700

    Task 11 advanced task

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -7,9 +7,9 @@
 
 int main(void)
 {
-	int ones = '0'
-	int tens = '0'
-	int hundreds = '0'
+	int ones = '0';
+	int tens = '0';
+	int hundreds = '0';
 
 	for (hundreds = '0'; hundreds <= '9'; hundreds++)
 	{
