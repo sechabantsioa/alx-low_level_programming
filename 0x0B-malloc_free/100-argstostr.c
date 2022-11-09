3625b821a05692725002265d8c7aa8e105027240
sechabantsioa <sechabantsioa@gmail.com>
2022-11-08 22:29:09 -0800

    Hot fix task 5 advanced

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -22,24 +22,25 @@ char *argstostr(int ac, char **av)
 		{
 			l++;
 		}
-		l += ac;
+	}
+	l += ac;
 
-		str = malloc(sizeof(char) * l + 1);
-		if (str == NULL)
+	str = malloc(sizeof(char) * l + 1);
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < ac; i++)
+	{
+		for (n = 0; av[i][n]; n++)
 		{
-			return (NULL);
+			str[r] = av[i][n];
+			r++;
 		}
-		for (i = 0; i < ac; i++)
-		{
-			for (n = 0; av[i][n]; n++)
-			{
-				str[r] = av[i][n];
-				r++;
-			}
-			if (str[r] == '\0')
-			{
-				str[r++] = '\n';
-			}
-		}
-		return (str);
+	}
+	if (str[r] == '\0')
+	{
+		str[r++] = '\n';
+	}
+	return (str);
 }
